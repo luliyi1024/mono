697d7c6d597abeb83872c0a8e33384ab2fa116bc
luliyi1024 <luliyi1024@gmail.com>
2012-02-22 13:31:24 +0800

    add HelloWorld project to test the mono.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestClass t = new TestClass();
+            int c = t.IntAdd(5,4);
+            t.PrintT<int>(c);
+        }
+    }
+
+    class TestClass
+    {
+        public int IntAdd(int a, int b)
+        {
+            return a + b;
+        }
+
+        public void PrintT<T>(T o)
+        {
+            System.Console.WriteLine(o.ToString());
+        }
+    }
+}
